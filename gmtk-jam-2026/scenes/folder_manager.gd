26e7dca47594c2264431186c2c15cca02fdbd3ca
Dherleth <erwan.moreira@gmail.com>
2026-07-25 20:47:10 +0200

    Folder manager wip

---
+++ gmtk-jam-2026/scenes/folder_manager.gd
@@ -0,0 +1,49 @@
+extends Control
+
+@onready var back_button: Button = $"../Root/BackButton"
+@onready var work_folder_content: ColorRect = $"../WorkFolderContent"
+@onready var important_folder_content: ColorRect = $"../ImportantFolderContent"
+@onready var not_important_folder_content: ColorRect = $"../NotImportantFolderContent"
+@onready var signed_contracts_folder_content: ColorRect = $"../WorkFolderContent/SignedContractsFolderContent"
+
+var back_goes_to := "root"
+
+func _ready() -> void:
+	back_button.hide()
+	work_folder_content.hide()
+	important_folder_content.hide()
+	not_important_folder_content.hide()
+	signed_contracts_folder_content.hide()
+
+
+func _on_work_button_pressed() -> void:
+	work_folder_content.show()
+	back_button.show()
+	
+	
+func _on_important_button_pressed() -> void:
+	important_folder_content.show()
+	back_button.show()
+	
+	
+func _on_not_important_button_pressed() -> void:
+	not_important_folder_content.show()
+	back_button.show()
+	
+
+func _on_signed_contracts_button_pressed() -> void:
+	back_goes_to = "work"
+	signed_contracts_folder_content.show()
+	
+
+func _on_back_button_pressed() -> void:
+	match back_goes_to:
+		"root":
+			back_button.hide()
+			work_folder_content.hide()
+			important_folder_content.hide()
+			not_important_folder_content.hide()
+		"work":
+			work_folder_content.show()
+			signed_contracts_folder_content.hide()
+			back_goes_to = 'root'
